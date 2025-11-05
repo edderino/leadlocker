da4c21c2447cd6b0c69d475f04cde88454a0167a
Adrian Morosin <theredincorporated@gmail.com>
2025-11-05 21:47:35 +0100

    refactor: remove NotificationManager, AISuggestions, and AdvancedAnalytics from client dashboard

--- src/components/client/DashboardClientRoot.tsx
+++ src/components/client/DashboardClientRoot.tsx
@@ -1,9 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import NotificationManager from './NotificationManager';
-import AISuggestions from './AISuggestions';
-import AdvancedAnalytics from './AdvancedAnalytics';
 import ClientDashboard from './ClientDashboard';
 
 interface Lead {
@@ -88,25 +85,5 @@ export default function DashboardClientRoot({ orgId }: DashboardClientRootProps)
     );
   }
 
-  return (
-    <>
-      {/* Push Notification Manager */}
-      <div className="mb-6">
-        <NotificationManager orgId={orgId} />
-      </div>
-
-      {/* AI Suggestions */}
-      <div className="mb-6">
-        <AISuggestions orgId={orgId} />
-      </div>
-
-      {/* Advanced Analytics Dashboard */}
-      <div className="mb-6">
-        <AdvancedAnalytics orgId={orgId} />
-      </div>
-
-      {/* Dashboard Content */}
-      <ClientDashboard leads={leads} orgId={orgId} />
-    </>
-  );
+  return <ClientDashboard leads={leads} orgId={orgId} />;
 }
